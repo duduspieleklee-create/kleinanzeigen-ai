b0657db41313b485e408681b77a4a5b8cf697b31
duduspieleklee-create <dudu.spiele.klee@gmail.com>
2026-06-28 07:23:09 +0200

    Create google.py

---
+++ app/api/auth/google.py
@@ -0,0 +1,17 @@
+from authlib.integrations.starlette_client import OAuth
+from starlette.config import Config
+import os
+
+config = Config(".env")  # or use os.getenv directly
+
+oauth = OAuth(config)
+
+oauth.register(
+    name='google',
+    client_id=os.getenv("GOOGLE_CLIENT_ID"),
+    client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={
+        'scope': 'openid email profile'
+    }
+)
