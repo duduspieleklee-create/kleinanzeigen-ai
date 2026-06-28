0d5bd6b34fbb6cf67fc0308c21c7c36b0cbcd840
duduspieleklee-create <dudu.spiele.klee@gmail.com>
2026-06-28 07:07:49 +0200

    Update variables.tf

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,16 +1,53 @@
-variable "aws_region" {
-  description = "AWS region to deploy into"
-  type        = string
-  default     = "eu-central-1"
-}
-
-variable "environment" {
-  description = "Deployment environment (dev, staging, prod)"
+variable "resource_group_name" {
+  description = "Name of the resource group"
   type        = string
 }
 
-variable "project" {
-  description = "Project name prefix for all resources"
+variable "location" {
+  description = "Azure region"
   type        = string
-  default     = "kleinanzeigen-ai"
+  default     = "westeurope"
+}
+
+variable "acr_name" {
+  description = "Name of the Azure Container Registry"
+  type        = string
+}
+
+variable "aks_cluster_name" {
+  description = "Name of the AKS cluster"
+  type        = string
+}
+
+variable "node_count" {
+  description = "Number of nodes in the default node pool"
+  type        = number
+  default     = 2
+}
+
+variable "vm_size" {
+  description = "VM size for AKS nodes"
+  type        = string
+  default     = "Standard_B2s"
+}
+
+variable "postgres_server_name" {
+  description = "Name of the PostgreSQL server"
+  type        = string
+}
+
+variable "postgres_admin_username" {
+  description = "PostgreSQL admin username"
+  type        = string
+}
+
+variable "postgres_admin_password" {
+  description = "PostgreSQL admin password"
+  type        = string
+  sensitive   = true
+}
+
+variable "redis_name" {
+  description = "Name of the Redis cache"
+  type        = string
 }
