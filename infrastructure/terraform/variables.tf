3ee5d4b3820a5be484c5277bb5f1a209e620238f
duduspieleklee-create <dudu.spiele.klee@gmail.com>
2026-06-28 08:00:41 +0200

    Update variables.tf

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,3 +1,9 @@
+variable "key_vault_name" {
+  description = "Name of the Azure Key Vault"
+  type        = string
+}
+
+
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
