e6c70a986d7e87a9e4daccb576dc5d84e591b997
duduspieleklee-create <dudu.spiele.klee@gmail.com>
2026-06-28 07:08:17 +0200

    Update outputs.tf

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -1,4 +1,15 @@
-output "environment" {
-  description = "Current deployment environment"
-  value       = var.environment
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "postgres_fqdn" {
+  value = azurerm_postgresql_flexible_server.db.fqdn
+}
+
+output "redis_hostname" {
+  value = azurerm_redis_cache.redis.hostname
 }
