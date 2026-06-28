cc6d4c7f2db862bace7e7e4ec465df397459d4ea
duduspieleklee-create <dudu.spiele.klee@gmail.com>
2026-06-28 08:00:16 +0200

    Update main.tf

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,3 +1,32 @@
+# Azure Key Vault
+resource "azurerm_key_vault" "main" {
+  name                        = var.key_vault_name
+  location                    = azurerm_resource_group.main.location
+  resource_group_name         = azurerm_resource_group.main.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get", "List", "Create", "Delete", "Purge", "Recover"
+    ]
+
+    secret_permissions = [
+      "Get", "List", "Set", "Delete", "Purge", "Recover"
+    ]
+  }
+}
+
+# Get current Azure client configuration
+data "azurerm_client_config" "current" {}
+
+
+
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
