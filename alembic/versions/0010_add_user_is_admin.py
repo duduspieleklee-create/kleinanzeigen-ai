afd4639a79953667011f9cfc2744ff44176c1245
duduspieleklee-create <dudu.spiele.klee@gmail.com>
2026-07-01 21:41:49 +0200

    security: add is_admin column migration (0010)

---
+++ alembic/versions/0010_add_user_is_admin.py
@@ -0,0 +1,32 @@
+"""add is_admin to users
+
+Revision ID: 0010
+Revises: 0009
+Create Date: 2026-07-01
+
+"""
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+
+revision: str = "0010"
+down_revision: Union[str, None] = "0009"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column(
+        "users",
+        sa.Column(
+            "is_admin",
+            sa.Boolean(),
+            nullable=False,
+            server_default=sa.text("false"),
+        ),
+    )
+
+
+def downgrade() -> None:
+    op.drop_column("users", "is_admin")
